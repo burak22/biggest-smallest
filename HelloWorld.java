b351d5dd75aa5ac12d8956afba80612b43afa28e
burak22 <43789237+burak22@users.noreply.github.com>
2022-09-12 15:32:37 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld{
         Scanner sc = new Scanner(System.in);
         System.out.print("Kaç sayı gireceksiniz? ");
         int countNumbers = sc.nextInt();
-        double smallest = Double.POSITIVE_INFINITY;
-        double biggest = Double.NEGATIVE_INFINITY;
-        for (int i = 1; i < countNumbers; i++) {
+        int smallest = Integer.MAX_VALUE;
+        int biggest = Integer.MIN_VALUE;
+        for (int i = 1; i <= countNumbers; i++) {
             System.out.print(i + ".  ");
             int number = sc.nextInt();
             smallest = (number<smallest)? number:smallest;
@@ -15,6 +15,5 @@ public class HelloWorld{
         System.out.println("En büyük: " + biggest);
         System.out.println("En küçük: " + smallest);
         sc.close();
-
     }            
 }
